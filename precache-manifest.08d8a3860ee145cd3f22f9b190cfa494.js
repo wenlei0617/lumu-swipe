2518cc4ae69845072265a5f952c54ac6a1cc03f7
DESKTOP-PGTK40G\wenle <784888856@qq.com>
2021-02-03 17:29:39 +0800

    Updates

---
+++ precache-manifest.08d8a3860ee145cd3f22f9b190cfa494.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34edf2bde549fb91b37501adae6bab7c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c4e856990345ccfa4043",
+    "url": "./static/css/main.d9334af2.chunk.css"
+  },
+  {
+    "revision": "c74f949e83b66a3bbe01",
+    "url": "./static/js/2.746dcd9d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.746dcd9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4e856990345ccfa4043",
+    "url": "./static/js/main.06559768.chunk.js"
+  },
+  {
+    "revision": "195d26a398a3287d9938",
+    "url": "./static/js/runtime-main.e8b559be.js"
+  }
+]);
